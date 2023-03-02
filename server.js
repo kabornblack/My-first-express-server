589347dcc5af387d75d6727e5bab627e68deb5ef
kabornblack <shaibu.kabiru@email.com>
2023-03-02 13:10:29 +0200

    Just started my first express server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response){
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1>Hello, World!</h1>");
 });
 
 app.listen(3000, function(){

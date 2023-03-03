93959449f130239259e811142c4e432126e7f3b5
kabornblack <shaibu.kabiru@email.com>
2023-03-03 13:08:35 +0200

    Added nodemon and let it work with different routes

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-  response.send("<h1>Hello, World!</h1>");
+app.get("/", function(req, res){
+  res.send("<h1>Hello, World!</h1>");
 });
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at 12345654");
+});
+
+app.get("/about", function(req, res){
+  res.send("My name is Shaibu Kabiru, from Nigeri and i am here in Estonia atm");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Coffee</li><li>code</li></ul>")
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000")

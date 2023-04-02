a7ebac1db375dfe7bdd02510819033238953ded9
kabornblack <shaibu.kabiru@email.com>
2023-04-02 23:18:22 +0300

    again

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
   res.send("<h1>Hello, World!</h1>");
 });
+function example() {
+  var a = 1;
+  let b = 2;
+  const c = 3;
+
+  if (true) {
+    var a = 4; // reassigns the value of a
+    let b = 5; // declares a new variable b with block-level scope
+    const c = 6; // declares a new constant c with block-level scope
+  }
+
+  console.log(a); // outputs 4
+  console.log(b); // outputs 2
+  console.log(c); // outputs 3
+}
+
+example();
 
 app.get("/contact", function(req, res){
   res.send("Contact me at 12345654 or kabornblack@gmail.com, email is fine if you need anything else");
